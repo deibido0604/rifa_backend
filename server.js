f769e64f01e7d93c6509c75d7ac12d002b639578
David Torres <davidjared1104@gmail.com>
2026-01-27 18:53:18 -0600

    Fix app.js for Vercel serverless

--- server.js
+++ server.js
@@ -1,26 +1,8 @@
 require('dotenv').config();
 
-const app = require('./app.js');
-const { connectDB } = require('./config/MDBConnection.js');
+const app = require('./app');
+const { connectDB } = require('./config/MDBConnection');
 
-const PORT = process.env.PORT || 3000;
+connectDB();
 
-const iniciarServidor = async () => {
-  try {
-    await connectDB();
-    app.listen(PORT, () => {
-      console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
-    });
-  } catch (error) {
-
-    if (process.env.NODE_ENV === 'production') {
-      app.listen(PORT, () => {
-        console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
-      });
-    } else {
-      process.exit(1);
-    }
-  }
-};
-
-iniciarServidor();
+module.exports = app;
